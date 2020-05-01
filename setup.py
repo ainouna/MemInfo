8a8de5c5779fd97e1026ed6fd98887d90fbe9ef2
ims <ims21@users.sourceforge.net>
2020-05-01 09:52:25 +0200

    Remove whitespaces

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.MemInfo'
 setup (name = 'enigma2-plugin-extensions-meminfo',
-       version = '1.04',
-       description = 'memory monitor',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['*.png', 'locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
-      )
+	version = '1.04',
+	description = 'memory monitor',
+	packages = [pkg],
+	package_dir = {pkg: 'plugin'},
+	package_data = {pkg: ['*.png', 'locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
+	cmdclass = setup_translate.cmdclass, # for translation
+	)
